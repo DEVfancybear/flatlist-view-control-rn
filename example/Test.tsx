606ff332204ee2c2bf832965974ad0047247c499
DuongLT <letrungduongxx3@gmail.com>
2021-04-30 20:24:39 +0700

    Update example: immutable state

--- example/Test.tsx
+++ example/Test.tsx
@@ -5,16 +5,21 @@ import {
 } from 'flatlist-view-control-rn';
 import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
+import {useImmer} from 'use-immer';
 import {getVideoList} from './API';
-
+type State = {
+  data: any[];
+};
 let pageSize = 10;
 export default function Test1() {
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useImmer<State>({data: []});
   const [currentPage, setCurrentPage] = useState(1);
   useEffect(() => {
     (async function () {
       let arr = await getVideoList(1, pageSize);
-      setData(arr);
+      setData(draft => {
+        draft.data = arr;
+      });
     })();
   }, []);
   const onFooterRefresh = async (endRefreshing: EndRefreshing) => {
@@ -23,10 +28,14 @@ export default function Test1() {
     let arr = await getVideoList(next, pageSize);
 
     if (arr.length === pageSize) {
-      setData(data.concat(arr));
+      setData(draft => {
+        draft.data.push(...arr);
+      });
       endRefreshing(RefreshState.CanLoadMore);
     } else {
-      setData(data.concat(arr));
+      setData(draft => {
+        draft.data.push(...arr);
+      });
       endRefreshing(RefreshState.NoMoreData);
     }
     setCurrentPage(next);
@@ -34,10 +43,14 @@ export default function Test1() {
   const onHeaderRefresh = async (endRefreshing: EndRefreshing) => {
     let arr = await getVideoList(1, pageSize);
     if (arr.length === pageSize) {
-      setData(arr);
+      setData(draft => {
+        draft.data = arr;
+      });
       endRefreshing(RefreshState.CanLoadMore);
     } else {
-      setData(arr);
+      setData(draft => {
+        draft.data = arr;
+      });
       endRefreshing(RefreshState.NoMoreData);
     }
     setCurrentPage(1);
@@ -48,7 +61,7 @@ export default function Test1() {
       <RefreshListView
         onFooterRefresh={onFooterRefresh}
         onHeaderRefresh={onHeaderRefresh}
-        data={data}
+        data={data.data}
         renderItem={({item}) => <Text style={{height: 50}}>{item.title}</Text>}
       />
     </View>
